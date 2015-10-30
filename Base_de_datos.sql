0cf69ff490334d931d3cb42a5003a416d2fe5619
JuanFcoPerez <juan.francisco.perez.cerda@gmail.com>
2015-10-30 13:13:07 -0300

    subo una bd de prueba como modificacion

--- Base_de_datos.sql
+++ Base_de_datos.sql
@@ -1,69 +1,68 @@
 /*
 SQLyog Community Edition- MySQL GUI v5.31
-Host - 6.0.4-alpha-community-log : Database - noticias
+Host - 5.6.26-log : Database - proyectowebupla
 *********************************************************************
-Server version : 6.0.4-alpha-community-log
+Server version : 5.6.26-log
 */
 
 /*!40101 SET NAMES utf8 */;
 
 /*!40101 SET SQL_MODE=''*/;
 
-create database if not exists `noticias`;
+create database if not exists `proyectowebupla`;
 
-USE `noticias`;
+USE `proyectowebupla`;
 
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-/*Table structure for table `clave` */
+/*Table structure for table `academico` */
 
-DROP TABLE IF EXISTS `clave`;
+DROP TABLE IF EXISTS `academico`;
 
-CREATE TABLE `clave` (
-  `Rut` int(9) NOT NULL,
-  `pass` varchar(100) NOT NULL,
-  PRIMARY KEY (`Rut`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `academico` (
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `rut` varchar(10) NOT NULL,
+  `contrasena` varchar(25) NOT NULL,
+  `estado` enum('Activo','Inactivo') NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  PRIMARY KEY (`rut`,`correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `clave` */
+/*Data for the table `academico` */
 
-/*Table structure for table `noticia` */
+/*Table structure for table `administrativo` */
 
-DROP TABLE IF EXISTS `noticia`;
+DROP TABLE IF EXISTS `administrativo`;
 
-CREATE TABLE `noticia` (
-  `Idnoticia` int(11) NOT NULL AUTO_INCREMENT,
-  `Titulo` varchar(100) NOT NULL,
-  `Subtitulo` varchar(500) NOT NULL,
-  `Cuerpo` varchar(500) NOT NULL,
-  `Imagen` varchar(500) NOT NULL,
-  `Sonido` varchar(500) DEFAULT NULL,
-  `Video` varchar(500) DEFAULT NULL,
-  `Rut` int(9) NOT NULL,
-  PRIMARY KEY (`Idnoticia`),
-  KEY `FK_Rut` (`Rut`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+CREATE TABLE `administrativo` (
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `contrasena` varchar(25) NOT NULL,
+  `estado` enum('Activo','Inactivo') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `noticia` */
+/*Data for the table `administrativo` */
 
-/*Table structure for table `usuario` */
+/*Table structure for table `alumno` */
 
-DROP TABLE IF EXISTS `usuario`;
+DROP TABLE IF EXISTS `alumno`;
 
-CREATE TABLE `usuario` (
-  `Rut` int(8) NOT NULL,
-  `Rutver` varchar(1) NOT NULL,
-  `Nombre` varchar(100) NOT NULL,
-  `Apellido1` varchar(100) NOT NULL,
-  `Apellido2` varchar(100) NOT NULL,
-  `Fechanac` date NOT NULL,
-  `Tipo` varchar(100) NOT NULL,
-  `Carrera` varchar(100) NOT NULL,
-  PRIMARY KEY (`Rut`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `alumno` (
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `rut` varchar(10) NOT NULL,
+  `carrera` varchar(50) NOT NULL,
+  `contrasena` varchar(25) NOT NULL,
+  `estado` enum('Activo','Inactivo') NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  PRIMARY KEY (`rut`,`correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `usuario` */
+/*Data for the table `alumno` */
+
+insert  into `alumno`(`nombre`,`apellido`,`rut`,`carrera`,`contrasena`,`estado`,`correo`) values ('Juan Francisco','Perez Cerda','18230912-5','informatica','juan1823','Activo','juan.perez.c@alumnos.upla.cl');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
